ca4d30c61e54a81d3882899a0de8c0539f82be37
jonatas-campista <jonatasc5622@gmail.com>
2024-01-15 09:58:05 -0300

    ajuste criptografia class user

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,17 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
 
+
 app = Flask(__name__)
 app.config.from_object('config')
 app.config['SECRET_KEY'] = 'secret'
 
-bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
+
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
 
 
--- app/models/tables.py
+++ app/models/tables.py
@@ -6,6 +6,7 @@ from flask_login import UserMixin
 def get_user (user_id):
     return User.query.filter_by(id =user_id).first()
 
+
 class User(db.Model, UserMixin):
     __tablename__ = "users"
 
@@ -20,24 +21,13 @@ class User(db.Model, UserMixin):
         self.name = name
 
     def _generate_password_hash(self, password):
-        # Gera um salt usando bcrypt.gensalt()
-        salt = bcrypt.gensalt()
-        if not salt:
-            raise ValueError("Failed to generate salt")
-
-        # Verifica se a senha fornecida é igual à senha de confirmação
-        if password != self.confirm_password:
-            raise ValueError("Passwords do not match")
-
-        # Faz o hash da senha com o salt gerado
+        # Gera o hash da senha usando Flask-Bcrypt
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        
-        if not hashed_password:
-            raise ValueError("Failed to generate hashed password")
-
         return hashed_password
+
     def verify_password(self, pwd):
         # Verifica se a senha fornecida coincide com a senha armazenada
         return bcrypt.check_password_hash(self.password, pwd)
+
     def __repr__(self):
-        return "<User %r>"% self.username
+        return "<User %r>" % self.username
